602b69e2f306e145a6f5757cd06229ebdccfb2e9
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2022-08-24 11:07:52 -0400

    Update CheckHyperThreading.ps1

--- CheckHyperThreading.ps1
+++ CheckHyperThreading.ps1
@@ -3,25 +3,35 @@
 # Putting it here for quick retrieve (and in case the page disappears :) )
 
 # Define variables
-$ComputerName = $env:COMPUTERNAME
+$ComputerNames = "E2016-01", "E2016-02"
 $LogicalCPU = 0
 $PhysicalCPU = 0
 $Core = 0
 
-# Get the Processor information from the WMI object
-$Proc = [object[]]$(get-WMIObject Win32_Processor -ComputerName $ComputerName)
- 
-#Perform the calculations
-$Core = $Proc.count
-$LogicalCPU = $($Proc | measure-object -Property NumberOfLogicalProcessors -sum).Sum
-$PhysicalCPU = $($Proc | measure-object -Property NumberOfCores -sum).Sum
-#Build the object
-$Hash = @{
-    LogicalCPU  = $LogicalCPU
-    PhysicalCPU = $PhysicalCPU
-    CoreNr      = $Core
-    HyperThreading = $($LogicalCPU -gt $PhysicalCPU)
+$Collection = @()
+Foreach ($ComputerName in $ComputerNames){
 
+    $LogicalCPU = 0
+    $PhysicalCPU = 0
+    $Core = 0
+    # Get the Processor information from the WMI object
+    $Proc = [object[]]$(get-WMIObject Win32_Processor -ComputerName $ComputerName)
+ 
+    #Perform the calculations
+    $Core = $Proc.count
+    $LogicalCPU = $($Proc | measure-object -Property NumberOfLogicalProcessors -sum).Sum
+    $PhysicalCPU = $($Proc | measure-object -Property NumberOfCores -sum).Sum
+    #Build the object
+    $Hash = @{
+        ComputerName = $ComputerName
+        LogicalCPU  = $LogicalCPU
+        PhysicalCPU = $PhysicalCPU
+        CoreNr      = $Core
+        HyperThreading = $($LogicalCPU -gt $PhysicalCPU)
+
+    }
+    #Output the object
+    $Collection += New-Object -TypeName PSObject -Property $Hash
 }
-#Output the object
-New-Object -TypeName PSObject -Property $Hash
+
+$Collection | ft
